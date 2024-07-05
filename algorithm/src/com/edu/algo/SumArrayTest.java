226096a8b0b9be1e015d25d76374f71043259a4b
WD <WD@WINDOWS-IH0D5O6>
2024-07-05 09:08:18 +0900

    0704

---
+++ algorithm/src/com/edu/algo/SumArrayTest.java
@@ -0,0 +1,45 @@
+package com.edu.algo;
+
+import java.util.Scanner;
+
+public class SumArrayTest {
+
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+		
+		int[] results = new int[10];
+		
+		for(int k = 0 ; k < 10 ; k++ ) {
+			sc.nextInt();
+			int[][] arr = new int[100][100];
+			int max = 0;
+			int width = 0;
+			int height = 0;
+			int right = 0;
+			int left = 0;
+			for(int i = 0 ; i < 100 ; i++) {
+				for(int j  = 0 ; j < 100 ; j++) {
+					arr[i][j] = sc.nextInt();
+				}
+			}
+			for(int i = 0 ; i < 100 ; i++) {
+				for(int j  = 0 ; j < 100 ; j++) {
+					width += arr[i][j]; 
+					height += arr[j][i];
+					if(i == j) right += arr[i][j];
+					if(i + j == 9) left += arr[i][j];
+				}
+				if(max <= width) max = width;
+				if(max <= height) max = height;
+				if(max <= right) max = right;
+			    if(max <= left) max = left;
+				width = 0;
+				height = 0;
+			}
+			results[k] = max;
+		}
+		for(int i = 0 ; i < 10 ; i++) System.out.println("#" + (i+1) + " " + results[i]);
+	}
+
+}
