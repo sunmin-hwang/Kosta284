6db3e6b2d42383e7a39c82a72d54f389c1c25d2c
sunmin_hwang <tommy08190@naver.com>
2024-09-03 17:57:33 +0900

    dfs

--- workshop09_SERVLET/src/main/java/com/edu/Kickboard.java
+++ workshop09_SERVLET/src/main/java/com/edu/Kickboard.java
@@ -1,7 +1,9 @@
 package com.edu;
+import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class Kickboard {
 
@@ -10,14 +12,31 @@ public class Kickboard {
 		static int[][] map;
 		static boolean[][] visited;
 
-		public static int execute(File path) throws FileNotFoundException {
+		public static int execute(File path) throws IOException {
 			//구현 하세요
+			BufferedReader br = new BufferedReader(new FileReader(path));
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int h = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
 			
+			map = new int[h][w];
+			visited = new boolean[h][w];
 			
+			for(int i = 0; i < h; i++) {
+				st = new StringTokenizer(br.readLine());
+				for(int j = 0; j < w; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			br.close();
 			return 0;
 		}
 		
-		public static void main(String[] args) throws FileNotFoundException {
+		public static void dfs() {
+			
+		}
+		
+		public static void main(String[] args) throws IOException {
 			System.out.println(execute(new File("input.txt")));
 		}
 
